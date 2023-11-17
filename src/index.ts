436207e35c5aeeb082ab869a2816da54e615f5a5
LucasV31 <146131921+LucasV31@users.noreply.github.com>
2023-11-17 18:50:56 +0000

    Deploying to gh-pages from master @ 21a53d987fc2f6de181d51f8d067babc5fa6faab 🚀

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
 
-import {bootstrapExtra} from "@workadventure/scripting-api-extra";
+import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+
+// Calling bootstrapExtra will initiliaze all the "custom properties"
+bootstrapExtra();
 
 // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
 bootstrapExtra().catch(e => console.error(e));
